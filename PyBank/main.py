ca36809246daf02f0801b7435e1dc3940bcfe842
gmnationse <gregnations@gmail.com>
2021-04-06 00:39:36 -0500

    new files

---
+++ PyBank/main.py
@@ -0,0 +1,10 @@
+import os
+import csv
+
+budget_data_csv = os.path.join("..", "Resources", "budget_data.csv")
+
+def sum (budget_data):
+    date = str(budget_data[0])
+    profit/loss = int(budget_data[1])
+  
+
